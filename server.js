6691473cb2ad96f57ebf6db8713487e5087172a0
Juliors1 <user@email.com>
2020-11-30 21:21:14 -0500

    fix bug loading up server

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const express = require("express");
 const path = require("path");
 const animalsDb = require("./data/animals");
 
+// create express app
+const app = express();
+
 // Configure the app (app.set)
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-// create express app
-const app = express();
 
 // Mount routes
 
@@ -24,7 +25,7 @@ app.get("/animals", function (req, res) {
 });
 
 // Tell the app to listen on port 3000
-// for HTTP requests from clients
+
 app.listen(3000, function () {
   console.log("Listening on port 3000");
 });
